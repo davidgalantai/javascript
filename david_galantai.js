29fc84d0919d5cc1aab5949afd416876e063df6b
davidgalantai <69991333+davidgalantai@users.noreply.github.com>
2020-12-07 16:08:49 +0100

    Add files via upload

--- david_galantai.js
+++ david_galantai.js
@@ -1,32 +1,75 @@
 "use strict"
+//user can only login with these names.
+let usernames = ["yazeen", "ec", "softhouse"];
+var form, input;
 
-var books = [];
+//Function that calls all other functions in order
+//--Don't modify--
+function init() {
+  initLoginForm();
+  initLabel();
+  initInput();
+  initButton();
+}
 
-function book(name, topic, pages, isbnNum){
-    this.name = name;
-    this.topic = topic;
-    this.pages = pages;
-    this.isbnNum = isbnNum;
-    this.getBookInfo = function(){
-        
-    };
-    console.log("book: " + this.name + "\nTopic: " + this.topic + "\nPages: " + this.pages + "\nISBN: " + this.isbnNum + "\n-----");
+function initLoginForm() {
+  //Add code here to:
+  //create and add form element to the Dom.
+  form = document.createElement("form");
+  document.body.appendChild(form);
+
+}
+
+function initLabel() {
+  //Add code here to:
+  //create label element and set the lable to "username" (TIP: use .textContent).
+  //Add it to the form element. (TIP: use .appendChild).
+  var label = document.createElement("label");
+  label.textContent = "username";
+  form.appendChild(label);
+}
+
+function initInput() {
+  //Add code here to:
+  //create input element and set it to be requried (TIP: use .required = true).
+  //Add it to the form element. (TIP: use .appendChild).
+  input = document.createElement("input");
+  input.required = true;
+  form.appendChild(input);
+}
+
+function initButton() {
+  //create button element and set the text to "login" (TIP: use .textContent)
+  ////Add it to the form element. (TIP: use .appendChild).
+  var button = document.createElement("button");
+  button.textContent = "login";
+  form.appendChild(button);
+  //Add click eventListener for button and call validateLoginInfo function inside it.
+  //if validateLoginInfo is true alert user with "Welcome" text.
+  //if validateLoginInfo is false add border style to the input element: "3px solid red". (TIP: use .style.border).
+
+  var defaultBorder = input.style.border;
+  button.addEventListener("click", function(e) {
+    e.preventDefault();
+    if (validateLoginInfo()) {
+      input.style.border = defaultBorder;
+      alert("Inlogged as " + input.value);
+    }
+    else {
+      input.style.border = "3px solid red";
+      alert("Wrong password")
+    }
+  });
 }
 
 
-books.push(new book("Harry Potter", "Magic", 470, "25252"));
-books.push(new book("Art of War", "Strategy", 320, "32323"));
-books.push(new book("Shining", "Horror", 400, "11114"));
+function validateLoginInfo() {
+  //Get the input using form input element (TIP: use .value)
+  //if the username is in the usernames array return true if not return false.
+  return usernames.includes(input.value);
+}
 
-function addBooks(){
-    var bookAdd = prompt("How many books do you want to add?", "Enter a numeric number");
-    
-    bookAdd = parseInt(bookAdd);
-
-    if (bookAdd){} // fånga error om bookAdd ej kan parseint
-    
-    for(var i = 0; i < bookAdd; i++){
-        books.push(new book(prompt("Enter book title"), prompt("Enter genre"), prompt("Enter pages"), prompt("Enter ISBN number")));
-    }
-};
-
+//Write code here to call the init function only after the dom has been loaded (TIP: check "DOMContentLoaded" event)
+window.addEventListener("DOMContentLoaded", () => {
+  init();
+});
